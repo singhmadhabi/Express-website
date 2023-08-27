ea942963cdd64392b9d0165b4034be54cf3cbfe0
Madhabi Singh <singhmadhabi4@gmail.com>
2023-08-28 04:59:21 +0545

    lear about URL building

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello get World");
+  resolve.send("Hello get World");
 });
 /*
 router.post("/", (req, res) => {
   res.send("Hello post World");
 });
 
-app.get("/:name", (req, res) => {
+*/
+router.get("/:name/:roll", (req, res) => {
   console.log(req.query);
   const { address } = req.query;
   const { name, roll } = req.params;
   res.send(`Hello ${name}. You are from ${address} & your roll is ${roll}`);
 });
-*/
 
 module.exports = router;
